92556dac8476de1d23f866ec42708aac8c87df7e
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-15 05:34:14 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental:{appDir: true},
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        statusCode: 301,
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const { blackA, violet, mauve } = require('@radix-ui/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './node_modules/tailwind-datepicker-react/dist/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...blackA,
+        ...violet,
+        ...mauve,
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
